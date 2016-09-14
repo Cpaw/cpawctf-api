708d81bb3f9f7972d242b017ba70e3a5556342d8
palloc <palloc.mknet10@gmail.com>
2016-09-14 14:07:03 +0900

    add some

--- score/models.py
+++ score/models.py
@@ -3,37 +3,36 @@ from django.db import models
 
 class Users(models.Model):
     """ User model """
+    user = models.OneToOneField(User)
     display_name = models.CharField(max_length=32)
     mail = models.EmailField()
     score = models.IntegerField(default=0)
-
     def __unicode__(self):
         return self.display_name
 
     
 class Category(models.Model):
     name = models.CharField(max_length=30)
-
     def __unicode__(self):
         return self.name
 
 class Challenge(models.Model):
     category = models.ForeignKey(Category)
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=50000)
+    description = models.TextField()
     data = models.ImageField(upload_to='data')
     score = models.IntegerField(default=0)
     flags = models.CharField(max_length=200)
-
     def __unicode__(self):
         return self.name
 
 class Notice(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length = 50000)
+    description = models.TextField()
     priority = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.title
-    
+
+
     
