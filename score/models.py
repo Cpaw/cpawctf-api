21b6bbfda9f01472778e63386ac587f2fabf0ff6
palloc <palloc.mknet10@gmail.com>
2016-09-14 13:46:15 +0900

    first commit

--- score/models.py
+++ score/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class User(models.Model):
+class Users(models.Model):
+    """ User model """
     display_name = models.CharField(max_length=32)
     mail = models.EmailField()
     score = models.IntegerField(default=0)
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
 
-    
