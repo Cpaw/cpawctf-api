ed3c4204caac1f478732bd7b0830919f5ce7498c
palloc <palloc.mknet10@gmail.com>
2016-09-16 11:09:53 +0900

    add profile view

--- score/views.py
+++ score/views.py
@@ -32,3 +32,7 @@ class NoticeViewSet(viewsets.ModelViewSet):
     serializer_class = NoticeSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
